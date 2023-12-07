1f8b5be382f3d444cf6fdcecbf0089e3977a7997
Landy510 <randy.chen@tpisoftware.com>
2023-12-08 00:41:48 +0800

    core: fundamental settings 1. add - tailwindCSS settings 2. add - global scss settings

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-[#50d71e]">
+        Hello world!
+      </h1>
+      <p className='test'>test</p>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    spacing: {
+      '1': '0.25rem',
+      '2': '0.5rem',
+      '2-1/2': '0.75rem',
+      '3': '1rem',
+      '4': '1.25rem',
+      '5': '1.5rem',
+      '6': '2rem',
+      '7': '2.25rem',
+      '8': '2.5rem',
+      '9': '3rem',
+      '10': '3.25rem',
+      '11': '3.5rem',
+      '12': '4rem',
+      '13': '4.25rem',
+      '14': '4.5rem',
+      '15': '5rem',
+    },
+    screens: {
+      'mobile': '375px',
+      'tablet': '768px',
+      'desktop': '1296px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
